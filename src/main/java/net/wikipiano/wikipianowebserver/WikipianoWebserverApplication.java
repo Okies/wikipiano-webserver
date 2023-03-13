6cd6abfe67d4574bec62154c31ae57c07475105d
Okies <ja00800@nate.com>
2023-03-13 18:37:43 +0900

    Initial commit

---
+++ src/main/java/net/wikipiano/wikipianowebserver/WikipianoWebserverApplication.java
@@ -0,0 +1,13 @@
+package net.wikipiano.wikipianowebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikipianoWebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikipianoWebserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/wikipiano/wikipianowebserver/WikipianoWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wikipiano.wikipianowebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikipianoWebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
